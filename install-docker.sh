88820434e13f9ef765cfb94e07a410944f2c3dc8
gomesuit <gomesuit>
2018-04-25 18:37:40 +0900

    Unsupported Docker version found [17.12.1-ce], supported versions are [1.11.x 1.12.x 1.13.x 17.03.x]

--- install-docker.sh
+++ install-docker.sh
@@ -4,5 +4,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 # apt-cache madison docker-ce
-apt-get install -y docker-ce=17.12.1~ce-0~ubuntu
+apt-get install -y docker-ce=17.03.2~ce-0~ubuntu-xenial
 /etc/init.d/docker start
